c30c1bd65957109bf95bcf3f63f9f125bfa38ce0
Ahmadreza Enayati <ahmadre147@gmail.com>
2023-06-30 12:23:08 +0330

    add traffic saving info to benchmark

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -2,16 +2,36 @@ import time
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import psutil
+
+
+def get_traffic():
+    traffic_sent = 0
+    traffic_received = 0
+    for interface in psutil.net_io_counters(pernic=True).values():
+        traffic_sent += interface.bytes_sent
+        traffic_received += interface.bytes_recv
+
+    return traffic_sent, traffic_received
 
 
 def request_to_dns_server(request_domain, dns_server):
     start_time = time.time()
     command = "nslookup " + request_domain + f" {dns_server}"
+    traffic_sent_0, traffic_received_0 = get_traffic()
+
     result = os.popen(command)
     output = result.read()
     end_time = time.time()
     result.close()
-    return end_time - start_time
+    print(request_domain + " done!")
+
+    traffic_sent_1, traffic_received_1 = get_traffic()
+
+    out_traffic = traffic_sent_1 - traffic_sent_0
+    in_traffic = traffic_received_1 - traffic_received_0
+
+    return (end_time - start_time), (out_traffic, in_traffic)
 
 
 # Run a shell command
@@ -19,17 +39,25 @@ requestsList = ["google.com", "stackoverflow.com", "gmail.com", "vc.kntu.ac.ir",
                 "golestan.kntu.ac.ir", "typingclub.com", "w3schools.com", "javatpoint.com"]
 
 benchmark_data_other_server = []
+traffic_other_server = []
 print("Start requesting through google dns server...")
-for num in range(0, 100):
-    benchmark_data_other_server.append(request_to_dns_server(requestsList[num % 10], "8.8.8.8"))
+for num in range(0, 10):
+    req = request_to_dns_server(requestsList[num % 10], "8.8.8.8")
+    benchmark_data_other_server.append(req[0])
+    traffic_other_server.append(req[1])
 print(">>> Finished requesting through google dns server")
 
 benchmark_data_our_server = []
+traffic_our_server = []
 print("Start requesting through our dns server...")
-for num in range(0, 100):
-    benchmark_data_our_server.append(request_to_dns_server(requestsList[num % 10], "127.0.0.1"))
+for num in range(0, 10):
+    req = request_to_dns_server(requestsList[num % 10], "127.0.0.1")
+    benchmark_data_our_server.append(req[0])
+    traffic_our_server.append(req[1])
 print(">>> Finished requesting through our dns server")
 
+print(f"incoming traffic saved: {sum([i[1] for i in traffic_our_server]) - sum([i[1] for i in traffic_other_server])}")
+print(f"outgoing traffic saved: {sum([i[0] for i in traffic_our_server]) - sum([i[0] for i in traffic_other_server])}")
 
 # Generate x-axis values
 x_values = np.arange(len(benchmark_data_other_server))
